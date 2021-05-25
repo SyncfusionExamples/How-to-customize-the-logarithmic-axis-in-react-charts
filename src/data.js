b283935432293af335e0d6e97d72a64be377d89f
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2021-05-25 19:12:04 +0530

    Adding React Charts Logarithmic Axis sample

---
+++ src/data.js
@@ -0,0 +1,14 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sampleData = [
+    { date: new Date(1995, 0, 1), value: 80 },
+    { date: new Date(1996, 0, 1), value: 200 },
+    { date: new Date(1997, 0, 1), value: 400 },
+    { date: new Date(1998, 0, 1), value: 600 },
+    { date: new Date(1999, 0, 1), value: 700 },
+    { date: new Date(2000, 0, 1), value: 1400 },
+    { date: new Date(2001, 0, 1), value: 2000 },
+    { date: new Date(2002, 0, 1), value: 4000 },
+    { date: new Date(2003, 0, 1), value: 6000 },
+    { date: new Date(2004, 0, 1), value: 8000 },
+    { date: new Date(2005, 0, 1), value: 11000 }
+];
